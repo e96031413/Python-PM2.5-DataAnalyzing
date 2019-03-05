0fd9b0f5ad17be127e6ab69378884d73271b571d
Yanwei Liu <e96031413@hotmail.com>
2019-03-05 21:46:27 +0800

    0305新增Scikit-Learn機器學習實作

--- 20181227-空氣盒子數據Line機器人爬蟲實作/PM2.5的Line機器人：使用Python + IFTTT 傳送最新PM2.5數值/PM25_Line_Sender.py
+++ 20181227-空氣盒子數據Line機器人爬蟲實作/PM2.5的Line機器人：使用Python + IFTTT 傳送最新PM2.5數值/PM25_Line_Sender.py
@@ -14,7 +14,7 @@ def send_ifttt(v1):   # 定義函式來向 IFTTT 發送 HTTP 要求
           '?value1='+str(v1))
     r = requests.get(url)      # 送出 HTTP GET 並取得網站的回應資料
     if r.text[:5] == 'Congr':  # 回應的文字若以 Congr 開頭就表示成功了
-        print('已傳送 (' +str(v1)+') 到 Line')
+        print('已傳送 ('+str(v1)+') 到 Line')
     return r.text
 
 ret = send_ifttt(i)  #傳送 HTTP 請求到 IFTTT
